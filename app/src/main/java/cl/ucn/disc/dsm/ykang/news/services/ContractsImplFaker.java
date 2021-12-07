ab3bd3c9107a0662b0dff40893a88f7e74ef1921
Jonathan-Kang <ymkang1630@gmail.com>
2021-12-07 10:47:02 -0300

    refactor(Pakages): create the pakages

--- app/src/main/java/cl/ucn/disc/dsm/ykang/news/services/ContractsImplFaker.java
+++ app/src/main/java/cl/ucn/disc/dsm/ykang/news/services/ContractsImplFaker.java
@@ -8,7 +8,7 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package cl.ucn.disc.dsm.ykang.news;
+package cl.ucn.disc.dsm.ykang.news.services;
 
 import com.github.javafaker.Faker;
 
@@ -17,6 +17,8 @@ import org.threeten.bp.ZonedDateTime;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cl.ucn.disc.dsm.ykang.news.model.News;
 
 /**
  *  Contract Implementation of News whit Faker
